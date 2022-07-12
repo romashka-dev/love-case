97e041bd2f466cb06af486ffd9bcb37881eb3cdc
romashka-dev <106475427+romashka-dev@users.noreply.github.com>
2022-07-13 00:19:42 +0300

    Developed header and footer
    - created responsive header for all devices
    - created burger menu
    - created responsive footer for all devices

--- src/js/app.js
+++ src/js/app.js
@@ -28,9 +28,9 @@ flsFunctions.isWebp();
 /* Добавление класса touch для HTML если браузер мобильный */
 // flsFunctions.addTouchClass();
 /* Добавление loaded для HTML после полной загрузки страницы */
-// flsFunctions.addLoadedClass();
+flsFunctions.addLoadedClass();
 /* Модуль для работы с меню (Бургер) */
-// flsFunctions.menuInit();
+flsFunctions.menuInit();
 /* Учет плавающей панели на мобильных устройствах при 100vh */
 // flsFunctions.fullVHfix();
 
